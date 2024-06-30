0663a60bcc5a4edc8bf0371ec2cf217be51d6309
Sanele Hlongwane <sanelehlongwane61@gmail.com>
2024-06-30 02:57:45 +0200

    new prisma

--- src/lib/checkUser.ts
+++ src/lib/checkUser.ts
@@ -1,5 +1,5 @@
 import { currentUser } from "@clerk/nextjs/server";
-import { db } from "./prisma";
+import { prisma } from "./prisma";
 
 export const checkUser = async () => {
   const user = await currentUser();
@@ -9,28 +9,28 @@ export const checkUser = async () => {
     return null;
   }
 
-  // Check if user is in db by Clerk ID
-  let loggedInUser = await db.user.findUnique({
+  // Check if user is in prisma by Clerk ID
+  let loggedInUser = await prisma.user.findUnique({
     where: {
       clerkId: user.id,
     },
   });
 
-  // If user is in db by Clerk ID
+  // If user is in prisma by Clerk ID
   if (loggedInUser) {
     return loggedInUser;
   }
 
-  // Check if user is in db by email
-  loggedInUser = await db.user.findUnique({
+  // Check if user is in prisma by email
+  loggedInUser = await prisma.user.findUnique({
     where: {
       email: user.emailAddresses[0].emailAddress,
     },
   });
 
-  // If user is in db by email, update Clerk ID and other details
+  // If user is in prisma by email, update Clerk ID and other details
   if (loggedInUser) {
-    loggedInUser = await db.user.update({
+    loggedInUser = await prisma.user.update({
       where: {
         email: user.emailAddresses[0].emailAddress,
       },
@@ -44,8 +44,8 @@ export const checkUser = async () => {
     return loggedInUser;
   }
 
-  // If user is not in db, create a new user
-  const newUser = await db.user.create({
+  // If user is not in prisma, create a new user
+  const newUser = await prisma.user.create({
     data: {
       clerkId: user.id,
       name: `${user.firstName} ${user.lastName}`,
